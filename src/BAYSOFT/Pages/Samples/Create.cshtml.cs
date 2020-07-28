716c98a47dff41ce068a3adcf572c0452acb5784
Ítalo Silveira <italobrian@gmail.com>
2020-07-28 18:13:09 -0300

    cqrslapp

---
+++ src/BAYSOFT/Pages/Samples/Create.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using BAYSOFT.Core.Application.Default.Samples.Commands.PostSample;
+using BAYSOFT.Infrastructures.Crosscutting;
+using BAYSOFT.Infrastructures.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Pages.Samples
+{
+    public class CreateModel : PageModelBase
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public CreateModel()
+        {
+
+        }
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<ActionResult> OnPost()
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Page();
+                }
+
+                var command = new PostSampleCommand();
+
+                command.Project(x => x.Description = Input.Description);
+
+                var response = await Mediator.Send(command);
+
+                if (response.StatusCode != 200)
+                {
+                    throw new BusinessException(response.Message);
+                }
+
+                return RedirectToPage("./Index");
+            }
+            catch(BusinessException bex)
+            {
+                ModelState.AddModelError("", bex.Message);
+                if (bex.InnerExceptions != null && bex.InnerExceptions.Count > 0)
+                {
+                    foreach(var innerException in bex.InnerExceptions)
+                    {
+                        ModelState.AddModelError("", innerException.Message);
+                    }
+                }
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return Page();
+            }
+        }
+
+        public class InputModel
+        {
+            [Required(AllowEmptyStrings = false, ErrorMessage = "{0} is required!")]
+            [Display(Name = "Description")]
+            [DataType(DataType.Text)]
+            [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} must be greater than {1} and smaller than {2}!")]
+            public string Description { get; set; }
+            public InputModel()
+            {
+
+            }
+        }
+    }
+}
